dcabd9ada9a7848c858d808e554107dd697c5919
caramirezal <cramireza@ciencias.unam.mx>
2017-06-03 10:24:24 -0500

    function for Berthomieux data minning

--- extractData.R
+++ extractData.R
@@ -1,34 +1,44 @@
 
-directory <- "Berthomieux2012/mainTextData/"
-fileNames <- list.files(directory)
-figNames <- sapply(fileNames,function(x) readLines(paste(directory, x, sep = ""),n=1))
-figNames <- sub(":.*","",figNames)
+## specific function defined for
+## data minning of the data of Berthomieux et al, 2013
+extractData <- function(directory,figure) {
+        ## locating and setting the data
+        fileNames <- list.files(directory)
+        figNames <- sapply(fileNames,function(x) readLines(paste(directory, x, sep = ""),n=1))
+        figNames <- sub(":.*","",figNames)
+        figNumber <- grep(figure,figNames)
+        filePath <- paste(directory,fileNames[figNumber],sep = "")
+        fileContent <- readLines(filePath)
+        
+        ## extracting flourescence data
+        floure <- grep("^Time,",fileContent)
+        emptyRow <- which(fileContent=="")
+        emptyRow <- emptyRow[1]
+        skipRows <- floure[1] + 1
+        rowNumber <- emptyRow - ( skipRows + 1 )
+        floure.df <- read.csv(filePath,header = FALSE,skip = skipRows,nrows = rowNumber)
+        assignedNames <- unlist(strsplit(fileContent[floure[1]],","))
+        names(floure.df) <- assignedNames
+        
+        ## extracting DO data
+        skipRows <- floure[2] + 1
+        DO.df <- read.csv(filePath,header = FALSE,skip = skipRows)
+        assignedNames <- unlist(strsplit(fileContent[floure[2]],","))
+        names(DO.df) <- assignedNames
+        
+        # plotting the data
+        par(mar=c(5,5,4,5))
+        with(DO.df, plot(Time,Absorbance,type="l",col="steelblue",lwd=2.5))
+        par(new=TRUE)
+        meanCol <- grep("mean",names(floure.df))
+        plot(floure.df$Time, floure.df[,meanCol],axes = FALSE,pch=20,
+             ylab = "",xlab = "")
+        axis(side=4)
+        
+}
 
-filePath <- paste(directory,fileNames[1],sep = "")
-fileContent <- readLines(filePath)
-floure <- grep("^Time,",fileContent)
-emptyRow <- which(fileContent=="")
-emptyRow <- emptyRow[1]
-skipRows <- floure[1] + 1
-rowNumber <- emptyRow - ( skipRows + 1 )
 
-## extracting flourescence data
-floure.df <- read.csv(filePath,header = FALSE,skip = skipRows,nrows = rowNumber)
-assignedNames <- strsplit(fileContent[floure[1]],",")
-assignedNames <- assignedNames[[1]]
-names(floure.df) <- assignedNames
-
-## extracting DO data
-skipRows <- floure[2] + 1
-DO.df <- read.csv(filePath,header = FALSE,skip = skipRows)
-assignedNames <- strsplit(fileContent[floure[2]],",")[[1]]
-names(DO.df) <- assignedNames
+#directory <- "Berthomieux2012/mainTextData/"
+#extractData(directory,"3A")
 
 
-par(mar=c(5,5,4,5))
-with(DO.df, plot(Time,Absorbance,type="l",col="steelblue",lwd=2.5))
-par(new=TRUE)
-meanCol <- grep("mean",names(floure.df))
-plot(floure.df$Time, floure.df[,meanCol],axes = FALSE,pch=20,
-     ylab = "",xlab = "")
-axis(side=4)
