8d4f85b03a0615460b71c146e67a614a7c3b00e0
caramirezal <cramireza@ciencias.unam.mx>
2017-06-03 10:33:36 -0500

    function for Berthomieux data minning

--- extractData.R
+++ extractData.R
@@ -1,7 +1,7 @@
 
 ## specific function defined for
 ## data minning of the data of Berthomieux et al, 2013
-extractData <- function(directory,figure) {
+extractData <- function(directory,figure,output="raw") {
         ## locating and setting the data
         fileNames <- list.files(directory)
         figNames <- sapply(fileNames,function(x) readLines(paste(directory, x, sep = ""),n=1))
@@ -26,19 +26,26 @@ extractData <- function(directory,figure) {
         assignedNames <- unlist(strsplit(fileContent[floure[2]],","))
         names(DO.df) <- assignedNames
         
-        # plotting the data
-        par(mar=c(5,5,4,5))
-        with(DO.df, plot(Time,Absorbance,type="l",col="steelblue",lwd=2.5))
-        par(new=TRUE)
-        meanCol <- grep("mean",names(floure.df))
-        plot(floure.df$Time, floure.df[,meanCol],axes = FALSE,pch=20,
-             ylab = "",xlab = "")
-        axis(side=4)
+        if ( output == "raw") {
+                return(list("flourescence"=floure.df,"DO"=DO.df))
+        }
+        if ( output == "plot") {
+                # plotting the data
+                par(mar=c(5,5,4,5))
+                with(DO.df, plot(Time,Absorbance,type="l",col="steelblue",lwd=2.5))
+                par(new=TRUE)
+                meanCol <- grep("mean",names(floure.df))
+                plot(floure.df$Time, floure.df[,meanCol],axes = FALSE,pch=20,
+                     ylab = "",xlab = "")
+                axis(side=4)
+                
+        }
         
+       
 }
 
-
-#directory <- "Berthomieux2012/mainTextData/"
-#extractData(directory,"3A")
+#rm(list = ls())
+directory <- "Berthomieux2012/mainTextData/"
+extractData(directory,"3A","plot")
 
 
